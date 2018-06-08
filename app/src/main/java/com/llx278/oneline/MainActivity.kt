0cd046411f6c3b7c97235dbb2bde38185886010b
Timmy <luili16@126.com>
2018-06-08 18:36:19 +0800

    first commit

---
+++ app/src/main/java/com/llx278/oneline/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.llx278.oneline
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
